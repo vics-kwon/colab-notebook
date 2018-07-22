5674fdcc8879917c5b0898834c75aff32d6ee8cc
vics <victor.rhkwon@gmail.com>
2018-07-22 16:55:31 +0900

    first commit

---
+++ baseline.py
@@ -0,0 +1,15 @@
+import opensim as osim
+from osim.env import ProstheticsEnv
+
+env = ProstheticsEnv(visualize=True)
+observation = env.reset()
+total_reward = 0.0
+for i in range(200):
+    # make a step given by the controller and record the state and the reward
+    observation, reward, done, info = env.step(env.action_space.sample())
+    total_reward += reward
+    if done:
+        break
+
+# Your reward is
+print("Total reward %f" % total_reward)
